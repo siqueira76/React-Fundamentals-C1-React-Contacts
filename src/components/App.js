625234344c60488cda93be8cb19897166f5f2c84
Carlos Siqueira <csiqueira@eb.com>
2022-09-18 11:39:16 -0300

    Moving Contacts into State

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,30 @@
+import { useState } from "react";
 import "../css/App.css";
 import ListContacts from "./ListContacts";
 
 const App = () => {
-  const contacts = [
-    {
-      id: "karen",
-      name: "Karen Isgrigg",
-      handle: "karen_isgrigg",
-      avatarURL: "http://localhost:5001/karen.jpg",
-    },
-    {
-      id: "richard",
-      name: "Richard Kalehoff",
-      handle: "richardkalehoff",
-      avatarURL: "http://localhost:5001/richard.jpg",
-    },
-    {
-      id: "tyler",
-      name: "Tyler McGinnis",
-      handle: "tylermcginnis",
-      avatarURL: "http://localhost:5001/tyler.jpg",
-    },
-  ];
+  const [contacts, setContacts] = useState(
+    [
+      {
+        id: "karen",
+        name: "Karen Isgrigg",
+        handle: "karen_isgrigg",
+        avatarURL: "http://localhost:5001/karen.jpg",
+      },
+      {
+        id: "richard",
+        name: "Richard Kalehoff",
+        handle: "richardkalehoff",
+        avatarURL: "http://localhost:5001/richard.jpg",
+      },
+      {
+        id: "tyler",
+        name: "Tyler McGinnis",
+        handle: "tylermcginnis",
+        avatarURL: "http://localhost:5001/tyler.jpg",
+      },
+    ]
+  );
 
   return (
     <div>
